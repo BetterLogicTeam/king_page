46ded048776b307765e98a93bf56406c91bc7d72
BetterLogic Team <102576102+BetterLogicTeam@users.noreply.github.com>
2022-10-19 15:13:20 +0500

    Minting Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 import Landing from './Components/Landing/Landing'
+import { useEffect } from 'react';
+// import { toast, ToastContainer } from 'react-toastify';
 
 function App() {
+
+  // const connectWallte = async () => {
+  //   try {
+  //     let acc
+    
+  //     if (acc == "No Wallet") {
+       
+  //       toast("No Wallet")  }
+  //     else if (acc == "Wrong Network") {
+        
+  //      } else {
+  //         let myAcc = acc?.substring(0, 4) + "..." + acc?.substring(acc?.length - 4);
+  //     }
+
+  //   } catch (e) {
+  //     console.log("Error while Connect Walte", e);
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   connectWallte()
+  // }, [])
+  
   return (
     <div className="App">
+       {/* <ToastContainer /> */}
       <Landing />
     </div>
   );
--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -1,14 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import { loadWeb3 } from "../apis/api";
+import { bnbContractAddress, bnbNftContractAbi } from "../utilies/constant";
 import "./Landing.css";
 
 function Landing() {
   let [value, setValue] = useState(1);
+  const [ValueBNB, setValueBNB] = useState("")
 
   const increaseValue = () => {
-    if (value < 10) {
-      setValue(++value);
-      console.log("setValue", value);
-    }
+    // if (value < 10) {
+    // }
+    setValue(++value);
+    console.log("setValue", value);
   };
 
   const decreaseValue = () => {
@@ -17,6 +21,95 @@ function Landing() {
       console.log("setValue", value);
     }
   };
+
+
+
+  const Mint_With_BNB = async () => {
+    let acc = await loadWeb3();
+
+    if (acc == "No Wallet") {
+      toast.error("No Wallet Connected")
+    }
+    else if (acc == "Wrong Network") {
+      toast.error("Wrong Newtwork please connect to test net")
+    } else {
+
+
+      try {
+        // setButtonOne("Please Wait While Processing")
+
+        const web3 = window.web3;
+        let nftContractOf = new web3.eth.Contract(bnbNftContractAbi, bnbContractAddress);
+        let mintingWirePrice
+        let own_Address = await nftContractOf.methods.owner().call()
+       
+        if (own_Address == acc) {
+          mintingWirePrice = 0;
+        } else {
+          mintingWirePrice = await nftContractOf.methods.minting_price().call()
+          mintingWirePrice = web3.utils.fromWei(mintingWirePrice);
+          setValueBNB(mintingWirePrice)
+          mintingWirePrice = parseFloat(mintingWirePrice);
+          mintingWirePrice = value * mintingWirePrice
+          mintingWirePrice = web3.utils.toWei(mintingWirePrice.toString());
+        }
+        // console.log("mintingWirePrice", mintingWirePrice);
+
+        let hash = await nftContractOf.methods.mint( value).send({
+          from: acc,
+          value: mintingWirePrice
+        })
+        toast.success("Transaction Confirmed")
+       
+
+
+      } catch (e) {
+        console.log("Error while minting ", e)
+        toast.error("Transaction failed")
+
+
+      }
+
+    }
+  }
+
+  const getValue=async()=>{
+    try{
+      const web3 = window.web3;
+        let nftContractOf = new web3.eth.Contract(bnbNftContractAbi, bnbContractAddress);
+       let mintingWirePrice = await nftContractOf.methods.minting_price().call()
+       mintingWirePrice = web3.utils.fromWei(mintingWirePrice);
+       setValueBNB(mintingWirePrice)
+       console.log("mintingWirePrice",mintingWirePrice);
+
+    }catch(e){
+      console.log("Error While get BNB value",e);
+    }
+  }
+  // const minting_live_price = async () => {
+  //   try {
+
+  //     const web3 = window.web3;
+  //     let nftContractOf = new web3.eth.Contract(bnbNftContractAbi, bnbContractAddress);
+  //     let Value_in_bnb = await nftContractOf.methods.minting_price().call()
+  //     Value_in_bnb = web3.utils.fromWei(Value_in_bnb);
+  //     setValueBNB(Value_in_bnb)
+
+
+
+  //   } catch (e) {
+  //     console.log("Erroe while get BNB value", e);
+  //   }
+  // }
+
+  useEffect(() => {
+    getValue()
+
+  }, [])
+  
+
+
+
   return (
     <>
       <div className="main_div">
@@ -35,11 +128,11 @@ function Landing() {
                 </div>
                 <div className="heding">
                   <h5 className="text-white">GENESIS KING CROWN</h5>
-                  <p>BNB</p>
+                  <p>{ValueBNB} BNB</p>
                 </div>
 
                 <div className="light">
-                  <img src="coin.jpeg" className="img-fluid " alt="" />
+                  <img src="coin.jpeg"  width="100%" alt="" />
                 </div>
                 <div className="blck d-flex justify-content-center">
                   <div className="btn plus" onClick={() => decreaseValue()}>
@@ -54,7 +147,7 @@ function Landing() {
                 </div>
 
                 <div className="mint" style={{ cursor: "pointer" }}>
-                  <img src="mint.png" alt="" />
+                  <img src="mint.png" alt="" onClick={()=>Mint_With_BNB()} />
                 </div>
               </div>
             </div>
